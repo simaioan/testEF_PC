bbd99037d446f55d6ac3553bf234c50962d64ef3
simaioan <simaioan@yahoo.com>
2017-05-21 13:58:11 +0300

    com2

---
+++ HR_Predare/frmTestGit.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HR_Predare
+{
+    public partial class frmTestGit : Form
+    {
+        public frmTestGit()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //si aici liniii de cod etc
+        }
+    }
+}
